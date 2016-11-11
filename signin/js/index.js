5a492afb6d9d59baa3afaa781c994b6c80453ba2
worstprogrammerCN <657278923@qq.com>
2016-11-12 01:27:34 +0800

    ..

--- signin/js/index.js
+++ signin/js/index.js
@@ -1,4 +1,10 @@
 $(function(){
+	var resetButtonPrepare = function(){
+		$("#reset").click(function(){
+			$("input").val("");
+		})
+	}
+
 	var uploadButtonPrepare = function(){
 		$("#upload").click(function(){
 			if (allInputAreValid())
@@ -8,6 +14,7 @@ $(function(){
 			}
 		})
 	}
+
 	var allInputAreValid = function(){
 		var username_re = /^[a-zA-Z]\w{5,17}$/;
 		var username = $("input[name='username']").val();
@@ -31,12 +38,21 @@ $(function(){
 		 && email_pass)
 			return true;
 		else{
-			$(".alert").html("");
+			var alert = $(".alert").html("");
+			var error_html = "";
 			if (!username_pass)
-				$("")
+				error_html += "<p><strong>用户名</strong>6~18位英文字母、数字或下划线，必须以英文字母开头</p>";
+			if (!id_pass)
+				error_html += "<p><strong>学号</strong>8位数字，不能以0开头</p>";
+			if (!phone_pass)
+				error_html += "<p><strong>电话</strong>11位数字，不能以0开头</p>";
+			if (!email_pass)
+				error_html += "<p><strong>邮箱</strong>格式不符合规范!</p>";
+			alert.attr("class", "alert alert-danger").html(error_html);
 		}
 	}
-
+	
+	resetButtonPrepare();
 	uploadButtonPrepare();
 	
 })
